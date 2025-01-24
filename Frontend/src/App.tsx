a685470ea6dc0441bc6f042f87ab3b6831cc26da
Your Name <abirdutta90990@gmail.com>
2025-01-24 18:46:08 +0530

    fixed alias import.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
-import Layout from './components/layouts/Layouts'
+import Layout from '@/components/layouts/Layouts'
 
 function App() {
   return (
